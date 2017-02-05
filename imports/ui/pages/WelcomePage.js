02de5eedd57881ce43dfbf600853a927bb728911
Hugh Willson <hugh@octonary.com>
2017-02-05 15:42:39 -0500

    Wired up new intro message on welcome page.

--- imports/ui/pages/WelcomePage.js
+++ imports/ui/pages/WelcomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 
 const WelcomePage = () => (
   <div className="welcome-page">
-    TODO - welcome page
+    <p>Welcome to the <strong>{Meteor.settings.public.title}</strong>!</p>
   </div>
 );
 
